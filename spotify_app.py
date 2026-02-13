6a3bc6d0dc537909733f60d8beafa4c0e2e247b2
Reef Loubani <reefloubani@Reefs-MacBook-Pro.local>
2026-02-13 13:14:33 -0700

    Add song search across all playlists

--- spotify_app.py
+++ spotify_app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, request, redirect, url_for, render_template
+from flask import Flask, session, request, redirect, url_for, render_template, jsonify
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', os.urandom(64))
 client_id = os.environ.get('SPOTIFY_CLIENT_ID', '6ea0978ad61247d7a2a8bbb0e417a943')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET', '660da264b0034a4499c4e3305dbd7f2e')
 redirect_uri = os.environ.get('REDIRECT_URI', 'http://127.0.0.1:5000/callback')
-scope = 'playlist-read-private'
+scope = 'playlist-read-private user-library-read'
 
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
@@ -71,6 +71,60 @@ def get_playlists():
                          playlists=playlists_info, 
                          username=username,
                          total=len(playlists_info))
+
+@app.route('/api/search-songs')
+def search_songs():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        return jsonify({'error': 'Not authenticated'}), 401
+    
+    query = request.args.get('q', '').lower()
+    if not query or len(query) < 2:
+        return jsonify({'results': []})
+    
+    all_songs = []
+    
+    # Get all playlists
+    playlists_results = sp.current_user_playlists(limit=50)
+    playlists = playlists_results['items']
+    
+    while playlists_results['next']:
+        playlists_results = sp.next(playlists_results)
+        playlists.extend(playlists_results['items'])
+    
+    # Get songs from each playlist
+    for playlist in playlists:
+        playlist_id = playlist['id']
+        playlist_name = playlist['name']
+        
+        try:
+            tracks_results = sp.playlist_tracks(playlist_id, limit=100)
+            tracks = tracks_results['items']
+            
+            # Paginate through all tracks in playlist
+            while tracks_results['next']:
+                tracks_results = sp.next(tracks_results)
+                tracks.extend(tracks_results['items'])
+            
+            for item in tracks:
+                if item['track'] and item['track']['name']:
+                    track = item['track']
+                    track_name = track['name'].lower()
+                    artist_name = track['artists'][0]['name'].lower() if track['artists'] else ''
+                    
+                    # Search by song name or artist name (case insensitive)
+                    if query in track_name or query in artist_name:
+                        all_songs.append({
+                            'name': track['name'],
+                            'artist': track['artists'][0]['name'] if track['artists'] else 'Unknown',
+                            'album': track['album']['name'],
+                            'image': track['album']['images'][0]['url'] if track['album']['images'] else None,
+                            'url': track['external_urls']['spotify'],
+                            'playlist': playlist_name
+                        })
+        except:
+            continue
+    
+    return jsonify({'results': all_songs})
 
 @app.route('/logout')
 def logout():
